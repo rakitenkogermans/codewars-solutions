5acf2d2c474f5ad2fecdc384dac69812118d8dbf
rakitenkogermans <rakitenko.germans@gmail.com>
2022-01-22 12:06:26 +0200

    Create phone number

---
+++ CreatePhoneNumber/app.js
@@ -0,0 +1,7 @@
+const createPhoneNumber = (numbers) => {
+    return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
