2b70f6f202e3e0979cd77eef7affc84a5f79223b
rakitenkogermans <rakitenko.germans@gmail.com>
2022-04-15 21:47:02 +0300

    Product of consecutive Fib numbers

---
+++ ProductOfConsecutiveFibNumbers/app.js
@@ -0,0 +1,17 @@
+const productFib = (prod) => {
+    let curr = 0;
+    let next = 1;
+    while (curr * next < prod) {
+        next = curr + next;
+        curr = next - curr;
+    }
+    return [curr, next, curr * next === prod];
+}
+
+console.log(productFib(4895)) // [55, 89, true])
+console.log(productFib(5895)) // [89, 144, false])
+console.log(productFib(74049690)) // [6765, 10946, true])
+console.log(productFib(84049690)) // [10946, 17711, false])
+console.log(productFib(193864606)) // [10946, 17711, true])
+console.log(productFib(447577)) // [610, 987, false])
+console.log(productFib(602070)) // [610, 987, true])
